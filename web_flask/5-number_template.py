bab57e9e1231af0e1b079ef3538801d95582425e
Ogundimine Aderonke Yetunde <107619226+Bussyslim@users.noreply.github.com>
2023-02-04 15:15:00 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,54 +1,79 @@
 #!/usr/bin/python3
-""" 5. Add fifth view func that displays HTML page if n is int """
-
-from flask import Flask
-from flask import render_template
+"""script that starts a Flask web application"""
 
 
+# import Flask class from flask module
+# import render_template for rendering templates to browser
+from flask import Flask, render_template
+
+# create an instance called app of the class by passong the __name__ variable
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """ Returns some text. """
+def index():
+    """display "Hello HBNB!"
+    Returns:
+        str: text on the index page
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hello():
-    """ Return other text. """
+def hbnb_route():
+    """display "HBNB"
+    Returns:
+        str: text on the page
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def c_route(text):
+    """display "C", followed by the value of the text variable
+    Args:
+        text (str): text to be served on the page
+    Returns:
+        str: text on the page
+    """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/')
+@app.route('/python', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+def python_route(text):
+    """display "Python", followed by the value of the text variable
+    Args:
+        text (str): text to be served on the page
+    Returns:
+        str: text on the page
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
+def number_route(n):
+    """display "n is a number" only if n is an integer
+    Args:
+        n (integer): number to be displayed on page
+    Returns:
+        str: text on the page
+    """
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
-    n = str(n)
-    return render_template('5-number.html', n=n)
+def number_template_route(n):
+    """display a HTML page only if n is an integer
+    H1 tag: "Number: n" inside the tag BODY
+    Args:
+        n (integer): number to be displayed on page
+    Returns:
+        str: text on the page
+    """
+    return render_template('5-number.html', num=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
